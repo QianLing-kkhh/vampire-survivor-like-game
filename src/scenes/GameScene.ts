16b2fd8ae3ce988c7cc7a3d2c316eb812239ba95
QianLing-kkhh <koukouhuanghao@gmail.com>
2026-05-31 23:47:59 +0900

    Create initial player in GameScene

--- src/scenes/GameScene.ts
+++ src/scenes/GameScene.ts
@@ -1,11 +1,32 @@
 import Phaser from 'phaser';
 
+import characters from '../data/characters.json';
+import { PlayerController } from '../player/PlayerController';
+import { PlayerHealth } from '../player/PlayerHealth';
+import { PlayerStats } from '../player/PlayerStats';
+
 export class GameScene extends Phaser.Scene {
+  private player?: PlayerController;
+  private playerHealth?: PlayerHealth;
+
   constructor() {
     super('GameScene');
   }
 
   create(): void {
-    this.add.text(40, 40, 'GameScene Ready');
+    const playerStats = PlayerStats.fromConfig(characters.default);
+    const centerX = this.scale.width / 2;
+    const centerY = this.scale.height / 2;
+
+    this.player = new PlayerController(this, playerStats, centerX, centerY);
+    this.playerHealth = new PlayerHealth(playerStats.maxHp);
+  }
+
+  update(_time: number, delta: number): void {
+    this.player?.update(delta);
+  }
+
+  getPlayerHealth(): PlayerHealth | undefined {
+    return this.playerHealth;
   }
 }
