1d3cc455f05da45a42c3ddf32dc004234bdb989e
QianLing-kkhh <koukouhuanghao@gmail.com>
2026-05-31 23:09:53 +0900

    Add Phaser game configuration

---
+++ src/game/GameConfig.ts
@@ -0,0 +1,21 @@
+import Phaser from 'phaser';
+import { BootScene } from '../scenes/BootScene';
+import { PreloadScene } from '../scenes/PreloadScene';
+import { GameScene } from '../scenes/GameScene';
+import { UIScene } from '../scenes/UIScene';
+import { ResultScene } from '../scenes/ResultScene';
+
+export const GameConfig: Phaser.Types.Core.GameConfig = {
+  type: Phaser.AUTO,
+  parent: 'app',
+  width: 1280,
+  height: 720,
+  backgroundColor: '#101018',
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: false
+    }
+  },
+  scene: [BootScene, PreloadScene, GameScene, UIScene, ResultScene]
+};
